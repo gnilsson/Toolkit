7dc09d08fcce3f50add327eda116a371b0604673
Gustav <g.nilsson1992@gmail.com>
2022-02-17 14:26:03 +0100

    add .sln & clean

--- GN.Toolkit/AsyncSemaphore.cs
+++ GN.Toolkit/AsyncSemaphore.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GN.Toolkit;
 
-namespace GN.Toolkit
+public class AsyncSemaphore
 {
-    internal class AsyncSemaphore
+    private readonly SemaphoreSlim _semaphore;
+
+    private int _count = 0;
+
+    public int Count => _count;
+
+    public AsyncSemaphore(int maxConcurrency = 1) => _semaphore = new(maxConcurrency, maxConcurrency);
+
+    public async Task<IDisposable> EnterAsync()
     {
+        await _semaphore.WaitAsync().ConfigureAwait(false);
+
+        return Disposable.Create(() => _semaphore.Release());
+    }
+
+    public async Task<IDisposable> EnterWithCountAsync()
+    {
+        _count++;
+
+        await _semaphore.WaitAsync().ConfigureAwait(false);
+
+        return Disposable.Create(() =>
+        {
+            _semaphore.Release();
+            _count--;
+        });
     }
 }

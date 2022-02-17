778a6be0b52c90ebab5954655eeb65fc54d852eb
Gustav <g.nilsson1992@gmail.com>
2022-02-17 18:18:39 +0100

    add ConfigureAwaitFalseExtensions

---
+++ GN.Toolkit/ConfigureAwaitFalseExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace GN.Toolkit;
+
+public static class ConfigureAwaitFalseExtensions
+{
+    public static ConfiguredTaskAwaitable CAF(this Task task) => task.ConfigureAwait(false);
+    public static ConfiguredTaskAwaitable<T> CAF<T>(this Task<T> task) => task.ConfigureAwait(false);
+    public static ConfiguredValueTaskAwaitable CAF(this ValueTask task) => task.ConfigureAwait(false);
+    public static ConfiguredValueTaskAwaitable<T> CAF<T>(this ValueTask<T> task) => task.ConfigureAwait(false);
+    public static ConfiguredCancelableAsyncEnumerable<T> CAF<T>(this ConfiguredCancelableAsyncEnumerable<T> task) => task.ConfigureAwait(false);
+    public static ConfiguredCancelableAsyncEnumerable<T> CAF<T>(this IAsyncEnumerable<T> task) => task.ConfigureAwait(false);
+}

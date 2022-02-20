f716983300114e0a826ddea196dfadcff8a20d6f
Gustav <g.nilsson1992@gmail.com>
2022-02-20 22:14:58 +0100

    add settingsbase with CRTP

---
+++ GN.Toolkit/SettingsBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GN.Toolkit;
+
+public abstract class SettingsBase<T> where T : SettingsBase<T>
+{
+    public T? Bind(IConfiguration configuration)
+    {
+        configuration.Bind(typeof(T).Name, (T)this);
+
+        return this as T;
+    }
+}
